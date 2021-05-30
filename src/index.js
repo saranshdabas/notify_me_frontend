6d2f2cb6f17c8929c9076de29782d1e52d700a36
Apple <sdjee2015@gmail.com>
2021-05-30 14:03:13 +0530

    Added meta data tag, temp soln for https

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <meta
+      http-equiv='Content-Security-Policy'
+      content='upgrade-insecure-requests'
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
